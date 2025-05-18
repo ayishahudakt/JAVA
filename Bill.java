2914ed4b7ed293bb8469d3c8a5811d94b0a7b75d
ayishahudakt <24mcag01@meaec.edu.in>
2025-05-18 21:43:15 +0530

    Update Bill.java

--- Bill.java
+++ Bill.java
@@ -38,30 +38,30 @@ public class Bill {
                 System.out.println("Enter Product Unit_Price: "); 
                 int d = sc.nextInt(); 
                 ProductB pb = new ProductB(a, b, c, d); 
-} 
-} 
-order[i][j] = pb; 
-order[i][j].total = order[i][j].calculate(); 
-for (int i = 0; i < n; i++) { 
-int sum = 0; 
-System.out.println("Order No: " + (i + 1)); 
-Date date = java.util.Calendar.getInstance().getTime(); 
-System.out.println(date); 
-System.out.println("-------------------------------------------------------------"); 
-System.out.printf("%5s %20s %25s %10s %10s", "Prod_Id", "Name", "Quantity", "Unit_Price", "Total"); 
-System.out.println(); 
-System.out.println("-------------------------------------------------------------"); 
-for (int j = 0; j < order[i].length; j++) { 
-System.out.printf("%5s %20s %25s %10s %10s", order[i][j].prod_id, order[i][j].name, 
-order[i][j].quantity, order[i][j].unit_price, order[i][j].total); 
-System.out.println(); 
-} 
-System.out.println("-------------------------------------------------------------");            
-for (int k = 0; k < order[i].length; k++) { 
-sum = sum + order[i][k].total; 
-} 
-System.out.println(" Net Amount : " + sum); 
-System.out.println("-------------------------------------------------------------"); 
-} 
-} 
+            } 
+        } 
+        order[i][j] = pb; 
+        order[i][j].total = order[i][j].calculate(); 
+        for (int i = 0; i < n; i++) { 
+            int sum = 0; 
+            System.out.println("Order No: " + (i + 1)); 
+            Date date = java.util.Calendar.getInstance().getTime(); 
+            System.out.println(date); 
+            System.out.println("-------------------------------------------------------------"); 
+            System.out.printf("%5s %20s %25s %10s %10s", "Prod_Id", "Name", "Quantity", "Unit_Price", "Total"); 
+            System.out.println(); 
+            System.out.println("-------------------------------------------------------------"); 
+            for (int j = 0; j < order[i].length; j++) { 
+                System.out.printf("%5s %20s %25s %10s %10s", order[i][j].prod_id, order[i][j].name, 
+                order[i][j].quantity, order[i][j].unit_price, order[i][j].total); 
+                System.out.println(); 
+            } 
+            System.out.println("-------------------------------------------------------------");            
+            for (int k = 0; k < order[i].length; k++) { 
+                sum = sum + order[i][k].total; 
+            } 
+            System.out.println(" Net Amount : " + sum); 
+            System.out.println("-------------------------------------------------------------"); 
+        } 
+    } 
 }
